bc0726606b36d4bd526b66b4f2b6f1aa435363bb
Saxo_Broko <saxonbrooker@gmail.com>
2019-12-16 18:53:23 +1100

    Added a service worker for caching
    Also adds offline support for PWA and home screen installs

---
+++ sw.js
@@ -0,0 +1,79 @@
+const CACHE = "tmt-precache";
+const precacheFiles = ["index.html", "404.html"];
+
+self.addEventListener("install", function (event) {
+  // console.log("Install Event processing");
+
+  // console.log("Skip waiting on install");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      // console.log("Caching pages during install");
+      return cache.addAll(precacheFiles);
+    })
+  );
+});
+
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  // console.log("Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) { 
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
+
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            // console.log("Network request failed and no cache." + error);
+          });
+      }
+    )
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
